82f4dbf77424935740b3104503af33fce6c560cc
solomonkassa <solomonmulu000@gmail.com>
2023-03-11 03:04:06 -0800

    element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    return my_list[idx]
+    for arr in range(len(my_list)):
+        if ((idx < 0) or (idx > len(my_list))):
+            return (None)
+        elif (idx == arr):
+            return (my_list[arr])
