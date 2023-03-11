adb404409608e2c9a7c959f9208690197c44d7d0
solomonkassa <solomonmulu000@gmail.com>
2023-03-11 04:35:56 -0800

    reverse

--- 0x03-python-data_structures/3-main.py
+++ 0x03-python-data_structures/3-main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
-
-my_list = [1, 2, 3, 4, 5]
-print_reversed_list_integer(my_list)
+def print_reversed_list_integer(my_list=[]):
+    if (my_list is not None):
+        my_list.reverse()
+        for arr in range(len(my_list)):
+            print("{:d}".format(my_list[arr]))
