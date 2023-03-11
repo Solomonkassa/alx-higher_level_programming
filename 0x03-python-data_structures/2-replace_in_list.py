125668652326423bb5741d67859e26dc3d30fecb
solomonkassa <solomonmulu000@gmail.com>
2023-03-11 04:06:50 -0800

    replace

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx < 0:
-        return my_list
-    elif idx > len(my_list) or idx == 0:
+    if idx > len(my_list) - 1 or idx < 0:
         return my_list
     else:
         my_list[idx] = element
