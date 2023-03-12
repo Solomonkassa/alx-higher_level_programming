d0587866667283ac33f20f02d457b8200b6fb410
solomonkassa <solomonmulu000@gmail.com>
2023-03-12 06:51:47 -0700

    update

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python3
 def no_c(my_string):
-    for n in range(len(my_string) - 2):
-        if my_string[n] == 'c' or my_string[n] == 'C':
-            my_string = my_string[:n] + "" + my_string[n+1:]
-    return my_string
+    result = ""
+    for n in range(len(my_string)):
+        if my_string[n] != "c" and my_string[n] != "C":
+            result += my_string[n]
+    return result
