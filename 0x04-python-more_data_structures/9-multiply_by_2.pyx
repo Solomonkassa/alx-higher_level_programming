84e3796bdc67828b62f7d5532910bdbc1737c629
Solomonkassa <118729276+Solomonkassa@users.noreply.github.com>
2023-03-15 14:37:06 -0400

    Update 9-multiply_by_2.pyx

--- 0x04-python-more_data_structures/9-multiply_by_2.pyx
+++ 0x04-python-more_data_structures/9-multiply_by_2.pyx
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    return {key: value * 2 for key, value in a_dictionary.items()}
+    tmp_dict = a_dictionary.copy()
+    for x in tmp_dict.keys():
+        tmp_dict[x] *= 2
+    return (tmp_dict)
