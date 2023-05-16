64d94e74ed88c967625a2003226f4d597e028bc7
Solomonkassa <118729276+Solomonkassa@users.noreply.github.com>
2023-05-16 14:35:56 -0400

    Update 0-list_databases.sql

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
--- Lists all databases of my MySQL server.
-=======
--- This script list all databases in MySQL server
->>>>>>> 18fe949a60695c3754e03c827740a2bedf1c7027
+-- This script list all databases in MySQL server.
 SHOW DATABASES;
