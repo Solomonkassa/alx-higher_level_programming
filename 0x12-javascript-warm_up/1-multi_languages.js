a2f646607c3068a4e3f624e824dd800e8b316076
Solomonkassa <118729276+Solomonkassa@users.noreply.github.com>
2023-06-12 17:47:19 +0300

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-// It prints 3 lines
-
-console.log('C is fun\nPython is cool\nJavascript is amazing');
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
