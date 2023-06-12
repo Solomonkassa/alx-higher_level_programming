60beb9521ea74d9db5b9158cf83b999b8d776d67
Solomonkassa <118729276+Solomonkassa@users.noreply.github.com>
2023-06-12 17:53:23 +0300

    Update 101-call_me_moby.js

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
-// executes x times a function.
-
 exports.callMeMoby = function (x, theFunction) {
-  for (let i = 0; i < x; i++) {
-    theFunction();
-  }
+  for (let i = 0; i < x; i++) theFunction();
 };
